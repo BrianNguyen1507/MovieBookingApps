61b7caf1a41166b361fa9e0a7801b3e7e669e926
lepodo123 <116814338+lepodo123@users.noreply.github.com>
2024-06-15 13:05:51 +0700

    feat add: CRUD FE food

---
+++ Website/AdminSite/service/food/addFood.js
@@ -0,0 +1,27 @@
+import { getUserToken } from "../authenticate/authenticate.js";
+
+const apiUrl = "http://localhost:8083/cinema/addFood";
+
+export async function addFood(food) {
+  try {
+    const tokenUser = await getUserToken();
+    const response = await $.ajax({
+      url: apiUrl,
+      method: "POST",
+      contentType: "application/json",
+      headers: {
+        Authorization: `Bearer ${tokenUser}`,
+      },
+      data: JSON.stringify(food),
+    });
+
+    if (response.code !== 1000) {
+      return response.message;
+    }
+
+    return true;
+  } catch (error) {
+    console.error("Exception:", error);
+    return error.responseJSON ? error.responseJSON.message : error.message;
+  }
+}
