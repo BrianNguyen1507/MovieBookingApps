d9c3459d1047087ecddcb0efd00c535c05389ff1
lepodo123 <116814338+lepodo123@users.noreply.github.com>
2024-07-13 13:25:25 +0700

    feat add swap delete schedule

--- Website/AdminSite/service/schedule/fetchSchedule.js
+++ Website/AdminSite/service/schedule/fetchSchedule.js
@@ -12,75 +12,90 @@ $(document).ready(function () {
 
   $dateInput.on("change", async function () {
     const selectedDate = $dateInput.val();
+    sessionStorage.setItem("date",selectedDate);
     console.log(roomValue);
     if (roomValue != "none") {
-      const schedules = await getAllScheduleByRoomAndDate(
-        roomValue,
-        selectedDate
-      );
-
-      const thead = document.querySelector("#schedule-table thead");
-      const tbody = document.querySelector("#schedule-table tbody");
-      thead.innerHTML = "";
-      tbody.innerHTML = "";
-      const row = document.createElement("tr");
-      row.classList.add("drop-row");
-      row.heigh = 100;
-      for (let i = 0; i < 7; i++) {
-        const cell = document.createElement("td");
-        cell.id = "col-" + i + "-row-add";
-        cell.classList.add("drop-column");
-        cell.textContent = "Add";
-        row.appendChild(cell);
-      }
-      tbody.appendChild(row);
-
-      schedules.forEach((schedule, indexColumn) => {
-        const column = document.createElement("th");
-        column.id = "column-" + indexColumn;
-        const date = document.createElement("h5");
-        date.textContent = formatDate(schedule.date);
-        column.appendChild(date);
-        thead.appendChild(column);
-
-        schedule.movieSchedule.forEach((movieSchedule, indexRow) => {
-          if (tbody.children.length - 1 == indexRow) {
-            const row = document.createElement("tr");
-            row.id = "row-" + indexRow;
-
-            for (let i = 0; i < 7; i++) {
-              const emptyCell = document.createElement("td");
-              emptyCell.id = "col-" + i + "-row-" + indexRow;
-              emptyCell.classList.add("drop-column");
-              row.appendChild(emptyCell);
-            }
-            tbody.appendChild(row);
-          }
-          const row = document.querySelector("#row-" + indexRow);
-          const cell = row.children[indexColumn];
-          console.log(cell);
-          if (cell) {
-            cell.innerHTML =
-              movieSchedule.timeStart +
-              "<br> - " +
-              movieSchedule.timeEnd +
-              "<br>" +
-              truncateText(movieSchedule.film.title,20) +
-              "<br>" +
-              movieSchedule.film.duration +
-              " phút" +
-              "<br>" +
-              formatDate(movieSchedule.film.releaseDate);
-            cell.classList.add("draggable");
-            cell.setAttribute("draggable", "true");
-          }
-        });
-      });
+      await loadingTable(roomValue,selectedDate);
     }
   });
 });
 
+export async function loadingTable(roomId,date){
+  const schedules = await getAllScheduleByRoomAndDate(
+    roomId,
+    date
+  );
 
+  const thead = document.querySelector("#schedule-table thead");
+  const tbody = document.querySelector("#schedule-table tbody");
+  thead.innerHTML = "";
+  tbody.innerHTML = "";
+  const row = document.createElement("tr");
+  row.classList.add("drop-row");
+  row.heigh = 100;
+  for (let i = 0; i < 7; i++) {
+    const cell = document.createElement("td");
+    cell.id = "col-" + i + "-row-add";
+    cell.classList.add("drop-column");
+    cell.textContent = "Add";
+    row.appendChild(cell);
+  }
+  tbody.appendChild(row);
+
+  schedules.forEach((schedule, indexColumn) => {
+    const column = document.createElement("th");
+    column.id = "column-" + indexColumn;
+    const date = document.createElement("h5");
+    date.textContent = formatDate(schedule.date);
+    column.appendChild(date);
+    thead.appendChild(column);
+
+    schedule.movieSchedule.forEach((movieSchedule, indexRow) => {
+      if (tbody.children.length - 1 == indexRow) {
+        const row = document.createElement("tr");
+        row.id = "row-" + indexRow;
+
+        for (let i = 0; i < 8; i++) {
+          const emptyCell = document.createElement("td");
+          if(i==7){
+            emptyCell.classList.add("delete-zone");
+            emptyCell.style.backgroundColor = "red";
+            const image = document.createElement("img");
+            image.classList.add("delete-zone");
+            image.src = "img/trash_icon.png";
+            image.width=50;
+            emptyCell.appendChild(image);
+          }
+          else{
+            emptyCell.id = "col-" + i + "-row-" + indexRow;
+            emptyCell.classList.add("drop-column");
+          }
+          row.appendChild(emptyCell);
+        }
+        tbody.appendChild(row);
+      }
+      const row = document.querySelector("#row-" + indexRow);
+      const cell = row.children[indexColumn];
+      if (cell) {
+        cell.setAttribute("data-id",movieSchedule.id);
+        cell.innerHTML =
+          movieSchedule.timeStart +
+          "<br> - " +
+          movieSchedule.timeEnd +
+          "<br>" +
+          truncateText(movieSchedule.film.title,20) +
+          "<br>" +
+          movieSchedule.film.duration +
+          " phút" +
+          "<br>" +
+          formatDate(movieSchedule.film.releaseDate);
+        cell.classList.add("draggable");
+        cell.setAttribute("draggable", "true");
+      }
+    });
+  });
+}
+ 
 export function formatDate(dateString) {
   const parts = dateString.split("-");
   return `${parts[2]}/${parts[1]}/${parts[0]}`;
--- Website/AdminSite/service/schedule/swapScheduleService.js
+++ Website/AdminSite/service/schedule/swapScheduleService.js
@@ -1,20 +1,14 @@
 import { getUserToken } from "../authenticate/authenticate";
 
-export async function addSchedule(roomId, filmId, date) {
-  const uri = "http://localhost:8083/cinema/addSchedule";
-  const body = JSON.stringify({
-    roomId: roomId,
-    filmId: filmId,
-    date: date,
-  });
+export async function swapSchedule(id, idSwap, date) {
+  const uri = `http://localhost:8083/cinema/swapSchedule?id=${id}&idSwap=${idSwap}&date=${date}`;
   const token = await getUserToken();
   const response = await fetch(uri, {
-    method: "POST",
+    method: "PUT",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-    body: body,
   });
   const responseData = await response.json();
 
