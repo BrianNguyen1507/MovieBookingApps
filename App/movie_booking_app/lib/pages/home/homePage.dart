6fa35980ea5c615714cd2b94f90962dd15c15327
Brian <Phihieu1507@gmail.com>
2024-07-18 12:22:28 +0700

    hot fix: listing movie schedule

--- App/movie_booking_app/lib/pages/home/homePage.dart
+++ App/movie_booking_app/lib/pages/home/homePage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_booking_app/constant/AppConfig.dart';
 import 'package:movie_booking_app/constant/AppStyle.dart';
-import 'package:movie_booking_app/constant/Appdata.dart';
 import 'package:movie_booking_app/models/food/food.dart';
 import 'package:movie_booking_app/models/movie/movie.dart';
 import 'package:movie_booking_app/modules/common/AutoScrolling.dart';
--- Website/AdminSite/service/schedule/selectionFilm.js
+++ Website/AdminSite/service/schedule/selectionFilm.js
@@ -1,22 +1,26 @@
 import { getAllFilm } from "./getAllFilm.js";
 
-
-export async function fetchingSeclectionFilm() {
+async function loadMovies() {
   const films = await getAllFilm();
-
   const movies = document.querySelector("#list-movie");
+
   films.forEach((film) => {
     const divPadding = document.createElement("div");
     divPadding.style.padding = "10px";
-    
-    divPadding.classList.add("col-12", "col-md-2", "d-flex", "justify-content-center","movie-item");
+    divPadding.classList.add(
+      "col-12",
+      "col-md-2",
+      "d-flex",
+      "justify-content-center",
+      "movie-item"
+    );
     divPadding.setAttribute("filmId", film.id);
     divPadding.setAttribute("draggable", true);
+
     const div = document.createElement("div");
-    
     div.id = "film-dragtable";
     div.setAttribute("draggable", true);
-    
+
     const poster = document.createElement("img");
     poster.src = `data:image/jpeg;base64,${film.poster}`;
     poster.height = 150;
@@ -25,18 +29,25 @@ export async function fetchingSeclectionFilm() {
 
     const title = document.createElement("p");
     title.textContent = film.title;
-    title.style.width ="100px";
+
     title.style.textAlign = "center";
     title.setAttribute("filmId", film.id);
 
     const duration = document.createElement("p");
-    duration.textContent = film.duration + " phút";
+    duration.textContent = `${film.duration} phút`;
     duration.style.textAlign = "center";
     duration.setAttribute("filmId", film.id);
+
+    const releaseDate = document.createElement("p");
+    releaseDate.textContent = `${film.releaseDate}`;
+    releaseDate.style.textAlign = "center";
+    releaseDate.setAttribute("filmId", film.id);
+
 
     div.appendChild(poster);
     div.appendChild(title);
     div.appendChild(duration);
+    div.appendChild(releaseDate);
     div.setAttribute("filmId", film.id);
 
     divPadding.appendChild(div);
@@ -44,11 +55,22 @@ export async function fetchingSeclectionFilm() {
   });
 }
 
+async function fetchingSelectionFilm() {
+  await loadMovies();
+  const observer = new IntersectionObserver({
+    root: document.querySelector(".infinite-row-container"),
+    rootMargin: "0px",
+    threshold: 1.0,
+  });
+
+  observer.observe(document.querySelector(".movie-item:last-child"));
+}
+
+fetchingSelectionFilm();
+
 export function truncateText(text, maxLength) {
   if (text.length > maxLength) {
     return text.substring(0, maxLength) + "...";
   }
   return text;
 }
-
-fetchingSeclectionFilm();
