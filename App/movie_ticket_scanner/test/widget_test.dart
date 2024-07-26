802b575d6bba5f433179e5bef06e2d770d5105c2
Brian <Phihieu1507@gmail.com>
2024-07-26 17:16:54 +0700

    feat: add qr code scanner

--- App/movie_ticket_scanner/test/widget_test.dart
+++ App/movie_ticket_scanner/test/widget_test.dart
@@ -13,8 +13,7 @@ import 'package:movie_ticket_scanner/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
+    await tester.pumpWidget(const QRScannPage());
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
